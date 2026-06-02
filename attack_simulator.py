231bd6ed92b8bef1eba92ca18cd6003256e8db95
CJAY6990 <car284jay@gmail.com>
2026-06-02 12:42:37 +0300

    Clean up attack simulator output and comments
    Removed demo attack generator print statements and comments.

--- attack_simulator.py
+++ attack_simulator.py
@@ -4,8 +4,6 @@ import requests
 
 BASE_URL = "http://127.0.0.1:5000"
 
-
-# fake users & attackers
 users = ["MEM001", "MEM002", "MEM003", "HACKER01", "ADMIN01"]
 passwords = ["wrong123", "1234", "admin", "password", "hack"]
 
@@ -41,9 +39,7 @@ def simulate_attack():
     except Exception as e:
         print("Server not running:", e)
 
-
-# continuous attack simulation
-print("🔥 DEMO ATTACK GENERATOR STARTED 🔥")
+print(" DEMO ATTACK GENERATOR STARTED ")
 
 while True:
     simulate_attack()
